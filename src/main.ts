a90e9c6740973ea863f908109193e1ecdd6d1aa1
Alumno <denis.com1243@gmail.com>
2026-03-03 12:49:15 -0500

    reeinstale modulos

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
